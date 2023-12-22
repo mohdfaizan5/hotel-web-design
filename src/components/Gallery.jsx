897d588065b035529db8cece0ba087990ae01657
Likitha N <likithanagaraj2004@gmail.com>
2023-12-22 12:23:39 +0530

    new_fea

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,9 +5,9 @@ import food from '../assets/Dinner.jpeg'
 function Gallery() {
   return (
     <div className='flex flex-col'>
-      <hr className='bg-black mt-8 border border-gray-300'/>
+      <hr className='bg-black mt-8 border border-black'/>
     
-      <div className='flex justify-center items-center flex-col  '>
+      <div className=' flex justify-center items-center flex-col  '>
         <h2 className='font-bold text-2xl mt-[50px]'>ROYAL EXPERIENCE</h2>
         <p className='max-w-[420px] mt-3 mb-[20px] text-gray-70000 ' >Designed by the most creative minds in proffesion,buility the finest people in the industry, and we have finally begun our journey too serve you. </p>
       </div>
--- src/components/features.jsx
+++ src/components/features.jsx
@@ -5,16 +5,18 @@ function features() {
   return (
    <>
    
-    <div className='flex flex-col justify-center items-center '>
+    <div className=' lg:flex-row lg:gap-[100px] flex flex-col justify-center items-center '>
       <div >
         <img className='w-[350px] h-[400px] rounded-3xl ' src={pic1} alt="" />
       </div>
-      <div className='font-bold text-2xl mt-[20px] ' >Royal Heritage</div>
+      <div>
+      <div className='font-bold text-2xl mt-[20px]  ' >Royal Heritage</div>
       <div className='max-w-[350px] mt-3 text-gray-70000 '>Taken from the Japenese culture, the possess a meaning of peacefull and tranquility.</div>
-      <div className='max-w-[350px] mt-[20px]'>
+      <div className='max-w-[350px] mt-[20px] '>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, debitis esse expedita fugiat necessitatibus, at maiores quibusdam, est laborum totam aliquid commodi. 
       </div>
       <div className='h-[40px] w-[110px] border text-center pt-[5px] mt-[10px] font-semibold border-slate-600'>Learn More</div>
+      </div>
     </div>
   
     <Gallery/>
